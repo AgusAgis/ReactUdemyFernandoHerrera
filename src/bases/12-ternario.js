1c254edd55f35a3fd3d2b0836475e152e9f0403f
Agustin Agis <desarrollo@carenasistemas.com.ar>
2023-12-14 12:54:46 -0300

    ternarios

---
+++ src/bases/12-ternario.js
@@ -0,0 +1,15 @@
+const activo = true;
+
+// let mensaje = '';
+// if(!activo){
+//     mensaje = 'Activo'
+// }else{
+//     mensaje = 'Inactivo'
+// }
+
+// const mensaje = (activo) ? 'Activo':'Inactivo'
+const mensaje = (activo) ? 'Activo': null
+//solo se ejecuta si las dos condiciones son verdad 
+//en este caso unicamente si se da true y sino NADA
+// const mensaje = activo && 'Activo'
+console.log(mensaje)
